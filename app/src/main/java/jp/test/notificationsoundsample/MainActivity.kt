5f801d0d3e2b2c94d2253c58253bb19b119f2f09
gnk263 <gnk263@users.noreply.github.com>
2020-03-01 18:15:29 +0900

    通知処理を実装

--- app/src/main/java/jp/test/notificationsoundsample/MainActivity.kt
+++ app/src/main/java/jp/test/notificationsoundsample/MainActivity.kt
@@ -1,12 +1,53 @@
 package jp.test.notificationsoundsample
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val CHANNEL_ID = "111"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createNotificationChannel()
+
+        notification_button.setOnClickListener {
+            val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("通知テスト")
+                .setContentText("寿限無寿限無五劫の擦り切れ...")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true)
+
+            with(NotificationManagerCompat.from(this)) {
+                notify(9999, builder.build())
+            }
+        }
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                "お知らせ",
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                description = "お知らせを通知します。"
+            }
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
